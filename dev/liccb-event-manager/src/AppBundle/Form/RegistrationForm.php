cd8a4c0e6e9dbe6b3f9279e76822b5ed3770c8ad
Jake Israel <jcmisrael@gmail.com>
2016-11-22 20:30:03 -0500

    Adds FormType includes and fixes mismatched closing brackets

--- dev/liccb-event-manager/src/AppBundle/Form/RegistrationForm.php
+++ dev/liccb-event-manager/src/AppBundle/Form/RegistrationForm.php
@@ -4,89 +4,91 @@ namespace AppBundle\Form;
 
 // src/AppBundle/Form/RegistrationForm.php
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationForm extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         switch ($options['flow_step']) {
-            case 1:
-                $builder->add('registrationType', ChoiceType::class, array(
-                    'choices' => array(
-                        'Participant' => 'participant',
-                        'Volunteer' => 'volunteer',
-                    ),
-                    'expanded' => true,
-                    multiple => false,
-                ));
-                break;
-            case 2:
-                $builder->add('eventSelection', ChoiceType::class, array(
-                    'choices' => array(
-                        'Test1',
-                    ),
-                ));
-                break;
-            case 3:
-                $builder->add('18OrOlder', ChoiceType::class, array(
-                    'choices' => array(
-                        'yes' => true,
-                        'no' => false,
-                    ),
-                ));
-                $builder->add('canSwim', ChoiceType::class, array(
-                    'choices' => array(
-                        'yes' => true,
-                        'no' => false,
-                    ),
-                ));
-                $builder->add('haveKayaked', ChoiceType::class, array(
-                    'choices' => array(
-                        'yes' => true,
-                        'no' => false,
-                    ),
-                ));
-                $builder->add('certifiedCPR', ChoiceType::class, array(
-                    'choices' => array(
-                        'yes' => true,
-                        'no' => false,
-                    ),
-                ));
-                break;
-            case 4:
-                $builder->add('fullName', TextType::class);
-                $builder->add('email', EmailType::class);
-                $builder->add('phoneNumber', TextType::class);
-                $builder->add('emergencyContactName', TextType::class);
-                $builder->add('emergencyContactNumber', TextType::class);
-                $builder->add('boatSeats', IntegerType::class);
-                $builder->add('pairingBoater', TextType::class);
-                break;
-            case 5:
-                $builder->add('eventDiscovery', ChoiceType::class, array(
-                    'choices' => array(
-                        'LICCB Calendar' => 'calendar',
-                        'Subscriber Newsletter or Direct Email' => 'subscriberLetter',
-                        'LICCB Blog' => 'blog',
-                        'Facebook' => 'facebook',
-                        'Twitter' => 'twitter',
-                        'Family or Friend' => 'familyorfriend',
-                        'Press Publication' => 'presspublication',
-                        'Web Search' => 'websearch',
-                        'Flier' => 'flier',
-                        'Saw us on the water' => 'sawonwater',
-                        'Just walking by' => 'walkingby',
-                        'Other' => 'other',
-                    ),
-                ));
-                break;
-        }
-
-        public function getBlockPrefix(){
-            return 'registration';
+	        case 1:
+		        $builder->add('registration_type', ChoiceType::class, array(
+			        'choices' => array(
+				        'Participant' => 'participant',
+				        'Volunteer' => 'volunteer',
+			        ),
+			        'expanded' => true,
+			        'multiple' => false,
+		        ));
+		        break;
+	        case 2:
+		        $builder->add('event_selection', ChoiceType::class, array(
+			        'choices' => array(
+				        'Test1',
+			        ),
+		        ));
+		        break;
+	        case 3:
+		        $builder->add('over18', ChoiceType::class, array(
+			        'choices' => array(
+				        'yes' => true,
+				        'no' => false,
+			        ),
+		        ));
+		        $builder->add('can_swim', ChoiceType::class, array(
+			        'choices' => array(
+				        'yes' => true,
+				        'no' => false,
+			        ),
+		        ));
+		        $builder->add('boat_experience', ChoiceType::class, array(
+			        'choices' => array(
+				        'yes' => true,
+				        'no' => false,
+			        ),
+		        ));
+		        $builder->add('cpr_certification', ChoiceType::class, array(
+			        'choices' => array(
+				        'yes' => true,
+				        'no' => false,
+			        ),
+		        ));
+		        break;
+	        case 4:
+		        $builder->add('full_name', TextType::class);
+		        $builder->add('email', EmailType::class);
+		        $builder->add('phone_number', TextType::class);
+		        $builder->add('emergency_contact_name', TextType::class);
+		        $builder->add('emergency_contact_number', TextType::class);
+		        $builder->add('boat_seats', IntegerType::class);
+		        $builder->add('pairing_boater', TextType::class);
+		        break;
+	        case 5:
+		        $builder->add('event_discovery', ChoiceType::class, array(
+			        'choices' => array(
+				        'LICCB Calendar' => 'calendar',
+				        'Subscriber Newsletter or Direct Email' => 'subscriberLetter',
+				        'LICCB Blog' => 'blog',
+				        'Facebook' => 'facebook',
+				        'Twitter' => 'twitter',
+				        'Family or Friend' => 'familyorfriend',
+				        'Press Publication' => 'presspublication',
+				        'Web Search' => 'websearch',
+				        'Flier' => 'flier',
+				        'Saw us on the water' => 'sawonwater',
+				        'Just walking by' => 'walkingby',
+				        'Other' => 'other',
+			        ),
+		        ));
+		        break;
         }
     }
 
+	public function getBlockPrefix(){
+		return 'registration';
+	}
 }
