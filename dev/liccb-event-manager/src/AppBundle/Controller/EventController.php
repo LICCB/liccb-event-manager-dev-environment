67facb0466b474bc9459c524c976108706c2022d
Jake Israel <jcmisrael@gmail.com>
2017-02-15 16:40:09 -0500

    Adds basic event editing
    Adds routing, controller, form, and view for editing event

--- dev/liccb-event-manager/src/AppBundle/Controller/EventController.php
+++ dev/liccb-event-manager/src/AppBundle/Controller/EventController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\EventEdit;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class EventController extends Controller
 {
@@ -10,10 +12,36 @@ class EventController extends Controller
     {
     	$event = $this->getDoctrine()
 		    ->getRepository('AppBundle:Event')
-		    ->find($id1);
+		    ->find($id);
 
         return $this->render('event/show.html.twig', array(
         'event' => $event
         ));
+    }
+
+    public function editAction(Request $request, $id){
+    	$event = $this->getDoctrine()
+		    ->getRepository('AppBundle:Event')
+		    ->find($id);
+
+    	$form = $this->createForm(EventEdit::class, $event);
+
+    	$form->handleRequest($request);
+
+    	if($form->isSubmitted() && $form->isValid()){
+    		$event = $form->getData();
+
+    		$em = $this->getDoctrine()->getManager();
+    		$em->persist($event);
+    		$em->flush();
+
+    		return $this->redirectToRoute('event_show',array(
+    			'id' => $id
+		    ));
+	    }
+
+	    return $this->render('event/edit.html.twig', array(
+	    	'form' => $form->createView(),
+	    ));
     }
 }
